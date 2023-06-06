debb9149f7fcae0445d640955880f1aab2242c63
samarthps29 <samarthps29@gmail.com>
2023-06-06 15:40:26 +0530

    Minor UI Changes

--- reflections-frontend/src/components/TodoPage.tsx
+++ reflections-frontend/src/components/TodoPage.tsx
@@ -1,6 +1,6 @@
-import Navigator from "./Navigator";
-import TodoUpdated from "./TodoCard";
 import styles from "../styles/background.module.css";
+import Navigator from "./Navigator";
+import TodoCard from "./TodoCard";
 
 const TodoPage = () => {
 	return (
@@ -20,8 +20,8 @@ const TodoPage = () => {
 				<div className="absolute left-0 top-0 h-full w-full border-l-4 border-stone-900"></div>
 				<Navigator />
 
-				<div className="overflow-x-none flex h-3/4 w-2/3 items-center justify-center rounded-lg bg-stone-400 bg-opacity-20 text-center font-inter text-5xl/[46px] font-bold tracking-tighter text-white ">
-					<TodoUpdated />
+				<div className="overflow-x-none flex h-3/4 w-2/3 items-center justify-center rounded-2xl bg-transparent">
+					<TodoCard />
 				</div>
 			</div>
 		</div>
