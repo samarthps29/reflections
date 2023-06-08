70991f9f946977d52472b0a5c79c41e7b9fd948b
samarthps29 <samarthps29@gmail.com>
2023-06-08 18:29:03 +0530

    Default fullscreen now has better UX

--- reflections-frontend/src/components/NotesPage.tsx
+++ reflections-frontend/src/components/NotesPage.tsx
@@ -8,24 +8,28 @@ const NotesPage = () => {
 	return (
 		<div className={`flex items-center bg-[#161616]`}>
 			<div
-				className={`flex flex-col items-center  ${
-					fullScreen ? "w-0" : "w-0 sm:w-1/4"
+				className={`flex w-0 flex-col items-center ${
+					fullScreen ? "" : "w-0 sm:w-1/4"
 				}`}
 			>
 				<div className="w-4/5">
-					<p className="text-center font-inter text-3xl/[30px] font-extrabold tracking-tighter text-[#537EC5] sm:text-4xl/[40px] lg:text-5xl/[50px]">
+					<p
+						className={`text-center font-inter text-3xl/[30px] font-extrabold tracking-tighter text-[#537EC5] opacity-0 sm:text-4xl/[40px] lg:text-5xl/[50px] ${
+							fullScreen ? "" : "opacity-100"
+						}`}
+					>
 						Journey through the Realm of Notes.
 					</p>
 				</div>
 			</div>
 			<div
-				className={`relative flex h-screen ${
-					fullScreen ? "w-full" : "w-full sm:w-3/4"
+				className={`relative flex h-screen w-full ${
+					fullScreen ? "" : "w-full sm:w-3/4"
 				} ${styles.bgi3} items-center justify-center`}
 			>
 				<div className="absolute left-0 top-0 h-full w-full border-l-4 border-stone-900"></div>
 				<Navigator setFullScreen={setFullScreen} />
-				<div className="z-10 flex h-5/6 w-11/12 items-center justify-center rounded-2xl bg-stone-400 bg-opacity-0 text-center font-inter text-5xl/[46px] font-bold tracking-tighter text-white">
+				<div className="z-10 flex h-5/6 w-11/12 items-center justify-center rounded-2xl bg-stone-600 text-center font-inter text-5xl/[46px] font-bold tracking-tighter text-white">
 					<NotesCard />
 				</div>
 			</div>
--- reflections-frontend/src/components/TodoPage.tsx
+++ reflections-frontend/src/components/TodoPage.tsx
@@ -8,25 +8,29 @@ const TodoPage = () => {
 	return (
 		<div className={`flex items-center bg-[#161616]`}>
 			<div
-				className={`flex flex-col items-center ${
-					fullScreen ? "w-0" : "w-0 sm:w-1/4"
+				className={`flex w-0 flex-col items-center ${
+					fullScreen ? "" : "w-0 sm:w-1/4"
 				}`}
 			>
 				<div className="w-4/5">
-					<p className="text-center font-inter text-3xl/[30px] font-extrabold tracking-tighter text-[#537EC5] sm:text-3xl/[30px] lg:text-5xl/[50px]">
+					<p
+						className={`text-center font-inter text-3xl/[30px] font-extrabold tracking-tighter text-[#537EC5] opacity-0 sm:text-3xl/[30px] lg:text-5xl/[50px] ${
+							fullScreen ? "" : "opacity-100"
+						}`}
+					>
 						Make Every Moment Matter.
 					</p>
 				</div>
 			</div>
 			<div
-				className={`relative flex h-screen ${
-					fullScreen ? "w-full" : "w-full sm:w-3/4"
+				className={`relative flex h-screen w-full ${
+					fullScreen ? "" : "w-full sm:w-3/4"
 				} ${styles.bgi4} items-center justify-center`}
 			>
 				<div className="absolute left-0 top-0 h-full w-full border-l-4 border-stone-900"></div>
 				<Navigator setFullScreen={setFullScreen} />
 
-				<div className="flex h-5/6 w-11/12 items-center justify-center rounded-2xl bg-stone-400 bg-opacity-0 text-center font-inter text-5xl/[46px] font-bold tracking-tighter text-white ">
+				<div className="flex h-5/6 w-11/12 items-center justify-center rounded-2xl bg-stone-600 text-center font-inter text-5xl/[46px] font-bold tracking-tighter text-white ">
 					<TodoCard />
 				</div>
 			</div>
