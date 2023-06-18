18b1946765aa46f789b6b770d993a535e5161563
samarthps29 <samarthps29@gmail.com>
2023-06-18 17:31:29 +0530

    Minor Improvements

--- reflections-frontend/src/components/DateSelector.tsx
+++ reflections-frontend/src/components/DateSelector.tsx
@@ -1,6 +1,7 @@
 import { ConfigProvider, DatePicker, theme } from "antd";
 import dayjs from "dayjs";
-import { useRef } from "react";
+import { useRef, useState } from "react";
+import { LeftIcon, RightIcon } from "../icons/SVGIcons";
 
 type DateSelectorProps = {
 	handleDateChange: (e: dayjs.Dayjs | null) => void;
@@ -8,40 +9,61 @@ type DateSelectorProps = {
 
 const DateSelector = ({ handleDateChange }: DateSelectorProps) => {
 	const datePickerRef = useRef<any>(null);
+	const [currDate, setCurrDate] = useState<dayjs.Dayjs>(dayjs());
 	const handleDateSelect = (date: dayjs.Dayjs | null) => {
 		handleDateChange(date);
 		datePickerRef.current?.blur();
+		setCurrDate(() => {
+			if (date === null) return dayjs();
+			else return date;
+		});
 	};
 	return (
-		<ConfigProvider
-			theme={{
-				algorithm: theme.darkAlgorithm,
-				token: {
-					colorInfo: "white",
-					colorPrimary: "#b9b0ff",
-					// colorTextBase: "white",
-					colorText: "#f4f4f4",
-					fontSize: 17,
-					colorBgElevated: "#1e1f22",
-					lineWidth: 0,
-					padding: 18,
-					paddingXS: 16,
-				},
-			}}
-		>
-			<DatePicker
-				className="rounded-lg bg-transparent font-bold hover:translate-y-0 scale-90 md:scale-100"
-				style={{ width: 129 }}
-				defaultValue={dayjs()}
-				format="DD-MMM-YYYY"
-				onChange={handleDateSelect}
-				ref={datePickerRef}
-				suffixIcon={null}
-				clearIcon={null}
-				bordered={false}
-				showToday={false}
-			/>
-		</ConfigProvider>
+		<div className="flex flex-row items-center justify-center">
+			<button
+				className="-mr-1 opacity-50"
+				style={{ marginTop: "0.13em" }}
+				onClick={() => handleDateSelect(currDate.add(-1, "day"))}
+			>
+				<LeftIcon />
+			</button>
+			<ConfigProvider
+				theme={{
+					algorithm: theme.darkAlgorithm,
+					token: {
+						colorInfo: "white",
+						colorPrimary: "#b9b0ff",
+						// colorTextBase: "white",
+						colorText: "#f4f4f4",
+						fontSize: 17,
+						colorBgElevated: "#1e1f22",
+						lineWidth: 0,
+						padding: 18,
+						paddingXS: 16,
+					},
+				}}
+			>
+				<DatePicker
+					className="scale-90 rounded-lg bg-transparent font-bold hover:translate-y-0 md:scale-100"
+					style={{ width: 129 }}
+					defaultValue={dayjs()}
+					format="DD-MMM-YYYY"
+					onChange={handleDateSelect}
+					ref={datePickerRef}
+					suffixIcon={null}
+					clearIcon={null}
+					bordered={false}
+					showToday={false}
+				/>
+			</ConfigProvider>
+			<button
+				className="opacity-50"
+				style={{ marginTop: "0.13em", marginLeft: "-.5rem" }}
+				onClick={() => handleDateSelect(currDate.add(+1, "day"))}
+			>
+				<RightIcon />
+			</button>
+		</div>
 	);
 };
 
--- reflections-frontend/src/icons/SVGIcons.tsx
+++ reflections-frontend/src/icons/SVGIcons.tsx
@@ -66,3 +66,41 @@ export const CrossIcon = () => {
 		</svg>
 	);
 };
+
+export const RightIcon = () => {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			fill="none"
+			viewBox="0 0 24 24"
+			strokeWidth={1.5}
+			stroke="white"
+			className="h-4 w-4"
+		>
+			<path
+				strokeLinecap="round"
+				strokeLinejoin="round"
+				d="M8.25 4.5l7.5 7.5-7.5 7.5"
+			/>
+		</svg>
+	);
+};
+
+export const LeftIcon = () => {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			fill="none"
+			viewBox="0 0 24 24"
+			strokeWidth={1.5}
+			stroke="white"
+			className="h-4 w-4"
+		>
+			<path
+				strokeLinecap="round"
+				strokeLinejoin="round"
+				d="M15.75 19.5L8.25 12l7.5-7.5"
+			/>
+		</svg>
+	);
+};
